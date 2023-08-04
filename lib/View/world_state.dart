f1460f42b2266fcacb995c7c4e69cb65ec1fcbe5
Shahanaj2000 <shahanajmt@gmail.com>
2023-08-04 15:27:42 +0530

    Covid-19 | SpashScreen|

---
+++ lib/View/world_state.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class WorldStateScreen extends StatelessWidget {
+  const WorldStateScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      body: Center(
+        child: Text('World State Screen'),
+      ),
+    );
+  }
+}
